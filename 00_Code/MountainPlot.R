faf24bbf7eec1a03edfec2b1a4ff09adc258f05d
DragonflyStats <kobriendublin@gmail.com>
2020-02-15 17:13:03 +0000

    Update MountainPlot.R

--- 00_Code/MountainPlot.R
+++ 00_Code/MountainPlot.R
@@ -93,3 +93,26 @@ p <- ggplot(myKMplot) +
     geom_histogram(aes(x = differences, y = ..density..), bins=15, fill = "grey", color = "black") + 
     geom_density(aes(x = differences))
 p
+
+library(tidyverse)
+vcf %>% mutate(diff = vcflong - vcfshort) %>% summarize(mean(diff))
+sd(diff)
+vcf %>% mutate(diff = vcflong - vcfshort) %>% summarize(sd(diff))
+vcf %>% mutate(diff = vcflong - vcfshort) %>% summarize(sd(vcflong),sd(vcfshort),sd(diff))
+
+
+ggplot(data=vcf) +stat_ecdf(data=vcf,aes(x=vcflong,col="red")) + 
+stat_ecdf(data=vcf,aes(x=vcfshort,col="blue"))
+
+#### VCF
+
+Method1 <- vcf$vcfshort
+Method2 <- vcf$vcflong
+Method1 <- Method1 - mean(Method1)
+Method2 <- Method2 - mean(Method2)
+Quant <- c(0:50/100,49:0/100)
+Method1 <- quantile(Method1,0:100/100)
+Method2 <- quantile(Method2,0:100/100)
+method <- c(rep("Method1",length(Method1)), rep("Method2",length(Method2))) %>% factor()
+myMountainPlot <- data.frame(value=c(Method1,Method2),quant =c(Quant,Quant),method)
+ggplot(data=myMountainPlot,aes(x=value,y=quant,col=method)) + geom_step(lwd=1.2) + theme_bw() + geom_vline(xintercept=0,col="red",lty=2) + geom_hline(yintercept=0)
