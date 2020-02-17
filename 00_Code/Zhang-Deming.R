60f476e086daba648771aeb9a18cdf8423915661
DragonflyStats <kobriendublin@gmail.com>
2020-02-17 11:09:15 +0000

    Update Zhang-Deming.R

--- 00_Code/Zhang-Deming.R
+++ 00_Code/Zhang-Deming.R
@@ -1,3 +1,40 @@
+
+ZhangData <- read.csv("//IXFILESVR01/Public/Common/GIS/Central_Resource/KOB_data_warehouse/08_TreeHeightModels/ZhangData.csv")
+
+MF <- ZhangData %>% dplyr::pull("MF")
+SV <- ZhangData %>% dplyr::pull("SV")
+
+library(MethComp)
+
+Deming(MF, SV,boot=TRUE)
+
+> Deming(MF, SV,boot=TRUE) %>% round(3)
+SV  = alpha + beta* MF 
+Estimate S.e.(boot)    50%    2.5% 97.5%
+Intercept   -4.122      6.508 -3.389 -16.303 9.020
+Slope        1.045      0.078  1.037   0.888 1.191
+sigma.MF     5.010      0.690  4.808   3.320 5.937
+sigma.SV     5.010      0.690  4.808   3.320 5.937
+
+ggplot(data =ZhangData, aes(x=MF,y=SV)) + 
+  geom_point(cex=2) + 
+  geom_smooth( method="lm",se= FALSE,linetype=3) + 
+  theme_bw() + 
+  geom_abline(intercept = -4.121610,slope=1.045132 ,col="red") + 
+  xlab("MF") + 
+  ylab("SV")
+
+> Deming(MF, SV,boot=TRUE,vr=2) %>% round(3)
+SV  = alpha + beta* MF 
+Estimate S.e.(boot)    50%    2.5%  97.5%
+Intercept   -2.362      6.540 -2.013 -14.274 12.149
+Slope        1.025      0.077  1.021   0.862  1.160
+sigma.MF     4.115      0.541  3.921   2.772  4.835
+sigma.SV     5.819      0.765  5.545   3.920  6.838
+
+https://mjskay.github.io/tidybayes/articles/tidy-brms.html
+
+
 X= c(47,66,68,69,70,70,73,75,79,81,85,87,87,87,90,100,104,105,112,120,132)	
 Y=c(43,70,72,81,60,67,72,72,92,76,85,82,90,96,82,100,94,98,108,131,131)	
 
